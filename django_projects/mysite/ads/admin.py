14bf9142c68eab360fe5bf0adc0f1445d183f9e2
mymello <nutsachikhladze@icloud.com>
2025-10-28 10:40:40 +0400

    forgot admin file

--- django_projects/mysite/ads/admin.py
+++ django_projects/mysite/ads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from ads.models import Ad
 
-# Register your models here.
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'owner', 'created_at']
+    list_filter = ['created_at', 'owner']
+    search_fields = ['title', 'text']
