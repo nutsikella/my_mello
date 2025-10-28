ff68450e8a84254f8a47c4bb894b77f77bccde31
mymello <nutsachikhladze@icloud.com>
2025-10-28 09:32:27 +0400

    module 5

--- dj4e-samples/zip/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.views.generic import TemplateView
-
-app_name='zip'
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='zip/main.html'), name='main'),
-    path('upload', views.UploadView.as_view(), name='upload'),
-]
