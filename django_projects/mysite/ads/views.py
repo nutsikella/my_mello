1243600b305c863951fa4f805f7123f5efe75bcd
mymello <nutsachikhladze@icloud.com>
2025-10-28 10:47:08 +0400

    eee

--- django_projects/mysite/ads/views.py
+++ django_projects/mysite/ads/views.py
@@ -1,5 +1,6 @@
 from ads.models import Ad
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from django.urls import reverse_lazy
 
 class AdListView(OwnerListView):
     model = Ad
@@ -13,12 +14,15 @@ class AdCreateView(OwnerCreateView):
     model = Ad
     fields = ['title', 'price', 'text']
     template_name = "ads/ad_form.html"
+    success_url = reverse_lazy('ads:all')  # Redirect to ads list after creation
 
 class AdUpdateView(OwnerUpdateView):
     model = Ad
     fields = ['title', 'price', 'text']
     template_name = "ads/ad_form.html"
+    success_url = reverse_lazy('ads:all')  # Redirect to ads list after update
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
     template_name = "ads/ad_confirm_delete.html"
+    success_url = reverse_lazy('ads:all')  # Redirect to ads list after deletion
